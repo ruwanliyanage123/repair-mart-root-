d0d4c53528ced14ae5d5c3cd9faa06bc149cf929
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-03-29 20:05:36 +0530

    Init commit

---
+++ src/main/java/com/repair/mart/root/repairmartroot/RepairMartRootApplication.java
@@ -0,0 +1,11 @@
+package com.repair.mart.root.repairmartroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairMartRootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RepairMartRootApplication.class, args);
+	}
+}
---
+++ src/test/java/com/repair/mart/root/repairmartroot/RepairMartRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repair.mart.root.repairmartroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairMartRootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

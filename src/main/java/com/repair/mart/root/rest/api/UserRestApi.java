550639bab2a577730b81c59ac76c29d5e035aeef
Ruwan <ruwanliyanagegalle1994@gmail.com>
2023-03-30 20:30:32 +0530

    Add postmapping

--- src/main/java/com/repair/mart/root/rest/api/UserRestApi.java
+++ src/main/java/com/repair/mart/root/rest/api/UserRestApi.java
@@ -1,6 +1,7 @@
 package com.repair.mart.root.rest.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,5 +16,6 @@ public interface UserRestApi {
      * @param requestMap requested map
      * @return response
      */
+    @PostMapping(path = "/signup")
     ResponseEntity<String> signUp(@RequestBody(required = true)Map<String, String> requestMap);
 }
